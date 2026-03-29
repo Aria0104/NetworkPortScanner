ad2b5d3e95b68ffb64be8fa75fa261924d0ce6cf
«Aria0104» <asy0107@mail.ru>
2026-03-29 15:04:18 +0300

    Первый коммит

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,98 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Threading;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetworkPortScanner
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> _openPorts = new ObservableCollection<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Start.Click += StartButton;
+            List.ItemsSource = _openPorts;
+        }
+
+        private async void StartButton(object? sender, RoutedEventArgs e)
+        {
+            string host = IP.Text;
+            if (!int.TryParse(DiapozonOt.Text, out int Ot) ||
+                  !int.TryParse(DiapozonDo.Text, out int Do) ||
+                    !int.TryParse(ColPotok.Text, out int Potoc))
+            {
+                return;
+            }
+
+            int totalPor = Do - Ot + 1;
+            int checkedPorts = 0;
+
+            SemaphoreSlim semaphore = new SemaphoreSlim(Potoc); // ограничивает количество параллельных задач
+            List<Task> tasks = new List<Task>();
+
+
+            for (int port = Ot; port <= Do; port++)
+            {
+                await semaphore.WaitAsync(); // если достигнут лимит поток ждем
+
+                int currentPort = port;
+
+                tasks.Add(Task.Run(async () =>
+                {
+                    try
+                    {
+                        if (await IsPortOpen(host, currentPort, 200)) 
+                        {
+                            await Dispatcher.UIThread.InvokeAsync(() =>
+                            {
+                                _openPorts.Add($"Порт {currentPort} открыт");
+                            });
+                        }
+                    }
+                    catch { }
+
+                    Interlocked.Increment(ref checkedPorts);
+
+                    await Dispatcher.UIThread.InvokeAsync(() => //это объект, который управляет главным UI-потоком, отправляет задачу в UI поток
+                    {
+                        ScanProgress.Value = (double)checkedPorts / totalPor * 100;
+                        StatusText.Text = $"Сканирование... Найдено: {_openPorts.Count}";
+                    });
+          
+                         semaphore.Release();  
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+            StatusText.Text = $"Завершено. Найдено: {_openPorts.Count}";
+        }
+
+
+        private async Task<bool> IsPortOpen(string host, int port, int timeout)
+        {
+            try
+            {
+                using TcpClient client = new TcpClient();
+
+                var task = client.ConnectAsync(host, port);
+                var delay = Task.Delay(timeout);
+
+                var completed = await Task.WhenAny(task, delay);
+
+                return completed == task && client.Connected;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
