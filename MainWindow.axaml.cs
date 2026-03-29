6d7905ba5e6e2fc901e72268d334fbdfdf00ee7a
Anna <asy0107@mail.ru>
2026-03-29 15:06:18 +0300

    Update MainWindow.axaml.cs

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -35,13 +35,13 @@ namespace NetworkPortScanner
             int totalPor = Do - Ot + 1;
             int checkedPorts = 0;
 
-            SemaphoreSlim semaphore = new SemaphoreSlim(Potoc); // ограничивает количество параллельных задач
+            SemaphoreSlim semaphore = new SemaphoreSlim(Potoc); 
             List<Task> tasks = new List<Task>();
 
 
             for (int port = Ot; port <= Do; port++)
             {
-                await semaphore.WaitAsync(); // если достигнут лимит поток ждем
+                await semaphore.WaitAsync(); 
 
                 int currentPort = port;
 
@@ -61,7 +61,7 @@ namespace NetworkPortScanner
 
                     Interlocked.Increment(ref checkedPorts);
 
-                    await Dispatcher.UIThread.InvokeAsync(() => //это объект, который управляет главным UI-потоком, отправляет задачу в UI поток
+                    await Dispatcher.UIThread.InvokeAsync(() => 
                     {
                         ScanProgress.Value = (double)checkedPorts / totalPor * 100;
                         StatusText.Text = $"Сканирование... Найдено: {_openPorts.Count}";
